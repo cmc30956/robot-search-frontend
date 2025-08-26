0fcca99cf0b39b27b1a2e99b4647b986604b230d
cmc30956 <zach.cai@foxmail.com>
2025-08-26 15:48:28 +0800

    Fix: Remove reportWebVitals as it's not needed for the app

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // 如果你的项目有这个文件，请保留
 import App from './App'; // 如果你的主组件是App.js，请保留
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-// 如果你想开始测量应用性能，请发送一个函数
-// 了解更多: https://bit.ly/CRA-vitals
-reportWebVitals();
+
 
